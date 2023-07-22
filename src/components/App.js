ac23e9ef52bdea31be81213831f307fd05e75272
1Markish <mark.kipchumba@student.moringaschool.com>
2023-07-22 06:17:40 +0300

    Done with assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,24 @@
-// create your App component here
+import { response } from "msw";
+import { useEffect, useState } from "react";
+
+function App() {
+  const [dogPhoto, setDogPhoto] = useState([]);
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  useEffect(() => {
+    fetch("https://dog.ceo/api/breeds/image/random")
+      .then((response) => response.json())
+      .then((data) => {
+        setDogPhoto(data.message);
+        setIsLoaded(true);
+      });
+  }, []);
+  if (!isLoaded) return <p>Loading...</p>;
+  return (
+    <>
+      <img src={dogPhoto} alt="A Random Dog" />
+    </>
+  );
+}
+
+export default App;
